d4237d5102da197541381ad032241863423024f0
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-01 00:57:15 +0700

    Add Splash Screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { timer } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(
@@ -18,10 +20,14 @@ export class AppComponent {
     this.initializeApp();
   }
 
+  showSplash = true;
+
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(4000).subscribe(() => (this.showSplash = false));
     });
   }
 }

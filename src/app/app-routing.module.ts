7ecc2e4d5c02cddfe6ae7b1d9d8a72092e9e4782
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-05 22:13:54 +0700

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'list-product',
+    loadChildren: () => import('./pages/outlet-product/list-product/list-product.module').then( m => m.ListProductPageModule)
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./pages/order/order.module').then( m => m.OrderPageModule)
+  },
+  {
+    path: 'detail-product',
+    loadChildren: () => import('./pages/outlet-product/detail-product/detail-product.module').then( m => m.DetailProductPageModule)
+  },
+  {
+    path: 'delivery-order',
+    loadChildren: () => import('./pages/outlet-product/delivery-order/delivery-order.module').then( m => m.DeliveryOrderPageModule)
   },
 ];
 

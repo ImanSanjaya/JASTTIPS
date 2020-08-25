571871135d23ef0c942e388d5eabc363ad853739
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-26 01:29:28 +0700

    Update: Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,48 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full",
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     loadChildren: () =>
-      import('./pages/dashboard/dashboard.module').then(
+      import("./pages/dashboard/dashboard.module").then(
         (m) => m.DashboardPageModule
       ),
   },
   {
-    path: 'list-product/3',
+    path: "list-product/3",
     loadChildren: () =>
-      import('./pages/order/order.module').then((m) => m.OrderPageModule),
+      import("./pages/order/order.module").then((m) => m.OrderPageModule),
   },
   {
-    path: 'list-product/:productId',
+    path: "list-product/:productId",
     loadChildren: () =>
-      import('./pages/outlet-product/list-product/list-product.module').then(
+      import("./pages/outlet-product/list-product/list-product.module").then(
         (m) => m.ListProductPageModule
       ),
   },
   {
-    path: 'order',
-    loadChildren: () =>
-      import('./pages/order/order.module').then((m) => m.OrderPageModule),
-  },
-  {
-    path: 'detail-product',
+    path: "detail-product/:detailProductId",
     loadChildren: () =>
       import(
-        './pages/outlet-product/detail-product/detail-product.module'
+        "./pages/outlet-product/detail-product/detail-product.module"
       ).then((m) => m.DetailProductPageModule),
   },
   {
-    path: 'delivery-order',
+    path: "order",
+    loadChildren: () =>
+      import("./pages/order/order.module").then((m) => m.OrderPageModule),
+  },
+  {
+    path: "delivery-order",
     loadChildren: () =>
       import(
-        './pages/outlet-product/delivery-order/delivery-order.module'
+        "./pages/outlet-product/delivery-order/delivery-order.module"
       ).then((m) => m.DeliveryOrderPageModule),
   },
 ];

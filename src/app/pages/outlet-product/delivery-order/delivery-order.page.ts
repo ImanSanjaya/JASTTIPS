e967f1c4c036a168af141247e1fde5c5a6972699
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-28 23:42:51 +0700

    Update: Rombak & load data dami

--- src/app/pages/outlet-product/delivery-order/delivery-order.page.ts
+++ src/app/pages/outlet-product/delivery-order/delivery-order.page.ts
@@ -1,26 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { JasttipsDataService } from 'src/app/api/jasttips-data.service';
+import { Component, OnInit } from "@angular/core";
+import { JasttipsDataService } from "src/app/api/jasttips-data.service";
+
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-delivery-order',
-  templateUrl: './delivery-order.page.html',
-  styleUrls: ['./delivery-order.page.scss'],
+  selector: "app-delivery-order",
+  templateUrl: "./delivery-order.page.html",
+  styleUrls: ["./delivery-order.page.scss"],
 })
 export class DeliveryOrderPage implements OnInit {
-  constructor(private jasttipsDataService: JasttipsDataService) {}
+  data: any;
 
-  ngOnInit() {}
+  idOutlet: any;
+  dataOutlet: any;
+
+  idOutletItem: any;
+  dataItem: any;
+
+  constructor(
+    private jasttipsDataService: JasttipsDataService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.getOutlet();
+  }
 
   formDeliveryOrder = {
-    name_customer: '',
-    phone_customer: '',
-    address_customer: '',
+    name_customer: "",
+    phone_customer: "",
+    address_customer: "",
+    additional_message: "",
   };
+
+  load(): any {
+    return this.http.get("assets/data/keranjang.json");
+  }
+
+  getDataOutlet() {
+    return this.load().pipe(
+      map((data: any) => {
+        return data.outlet;
+      })
+    );
+  }
+
+  getOutlet() {
+    this.getDataOutlet().subscribe((outlet: any) => {
+      this.dataOutlet = outlet;
+    });
+  }
 
   submitDeliveryOrder() {
     open(
       this.jasttipsDataService.sendMessageForDeliveryOrder(
-        '6287879571222',
+        "6287869667004",
         this.formDeliveryOrder
       )
     );

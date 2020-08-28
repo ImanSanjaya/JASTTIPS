e96dbccf432e7d20faff44d03ea5e710aa694f00
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-28 23:43:38 +0700

    Update: sendMessageForDeliveryOrder() masih statis

--- src/app/api/jasttips-data.service.ts
+++ src/app/api/jasttips-data.service.ts
@@ -33,19 +33,45 @@ export class JasttipsDataService {
   }
 
   public sendMessageForDeliveryOrder(phone_admin: string, formDeliveriOrder) {
+
+    const headerMsg = '*Untuk Pemesanan Item* ';
+    const nama = '*Nama :* ';
+    const noTelp = '*No* *Telepon* *:* ';
+    const alamat = '*Alamat* *:* ';
+    const pesanTambahan = '*Pesan* *Tambahan* *:* ';
+
+    const varCategory = '*( Makanan )*';
+
+    const nameOutlet = '*Kopi Kenangan*';
+    const alamatOutlet = 'Alamat Outlet : Grand Taruma';
+    const noTelpOutlet = 'No Telp : ';
+    const qty = '2';
+    const nameItem = 'Kopi Peka';
+    const priceItem = '(Rp.100,000)';
+
+    const subTotal = '*Sub Total : Rp.200,000*';
+
+    const total = '*TOTAL PEMESANAN : Rp.200,000*';
+
+
+    const enter = '%0A';
+    const enter2x = '%0A%0A';
+
     return (
       environment.apiSendMessageWA +
-      '/send?phone=' +
-      phone_admin +
-      '&text=' +
-      '*Nama%20:*%20' +
-      formDeliveriOrder.name_customer +
-      '%0A%0A' +
-      '*Nomor*%20*Telepon*%20*:*%20' +
-      formDeliveriOrder.phone_customer +
-      '%0A%0A' +
-      '*Alamat*%20*:*%20' +
-      formDeliveriOrder.address_customer
+      phone_admin + '&text=' +
+      headerMsg + enter2x +
+      nama + formDeliveriOrder.name_customer + enter +
+      noTelp + environment.apiSendMessageWA + formDeliveriOrder.phone_customer + enter +
+      alamat + formDeliveriOrder.address_customer + enter +
+      pesanTambahan + formDeliveriOrder.additional_message + enter2x +
+      varCategory + enter2x + 
+      nameOutlet + enter +
+      alamatOutlet + enter +
+      noTelpOutlet + environment.apiSendMessageWA + '6287879571222' + enter +
+      qty + ' ' + nameItem + ' ' + priceItem + enter + 
+      subTotal + enter2x + 
+      total
     );
   }
 

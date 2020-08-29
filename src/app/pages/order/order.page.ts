3af92818f74ff447d1771978ece32602309b2bc4
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-08-30 05:37:40 +0700

    Update: Order

--- src/app/pages/order/order.page.ts
+++ src/app/pages/order/order.page.ts
@@ -1,28 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { JasttipsDataService } from '../../api/jasttips-data.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { JasttipsDataService } from "../../api/jasttips-data.service";
 
 @Component({
-  selector: 'app-order',
-  templateUrl: './order.page.html',
-  styleUrls: ['./order.page.scss'],
+  selector: "app-order",
+  templateUrl: "./order.page.html",
+  styleUrls: ["./order.page.scss"],
 })
 export class OrderPage implements OnInit {
-  constructor(private jasttipsDataService: JasttipsDataService) {}
+  category: any;
 
-  ngOnInit() {}
+  constructor(
+    private jasttipsDataService: JasttipsDataService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.jasttipsDataService.getListCategory().subscribe((rest) => {
+      if (rest && rest.category) {
+        for (const listProduct of rest.category) {
+          if (listProduct && listProduct.id_category_outlet == 2) {
+            const idProduct = listProduct.id_category_outlet;
+            if (idProduct == idProduct) {
+              this.jasttipsDataService
+                .getListOutlet(3)
+                .subscribe((rest: any) => {
+                  this.category = listProduct.name_category_outlet;
+                });
+            }
+          }
+        }
+      }
+    });
+  }
 
   formOrder = {
-    name_customer: '',
-    phone_customer: '',
-    address_customer: '',
-    product_customer: '',
-    outlet_customer: '',
+    name_customer: "",
+    phone_customer: "",
+    address_customer: "",
+    product_customer: "",
+    outlet_customer: "",
   };
 
   submitOrder() {
     open(
       this.jasttipsDataService.sendMessageForOrder(
-        '6287879571222',
+        "6287879571222",
         this.formOrder
       )
     );

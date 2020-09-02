c7e8308d1ef121a8ceefe4b5d98ccec324ae73dc
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-09-03 03:07:02 +0700

    Update: Dashboard

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JasttipsDataService } from '../../api/jasttips-data.service';
+import { UserData } from 'src/app/api/user-data';
+import { ModalController } from '@ionic/angular';
+import { AccountPage } from '../account/account.page';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,12 +16,22 @@ export class DashboardPage implements OnInit {
 
   listCategory: any;
 
-  constructor(private jasttipsDataService: JasttipsDataService) {}
+  idOutlet: any;
+  itemPromo: any;
+
+  constructor(
+    private jasttipsDataService: JasttipsDataService,
+    private modalCtrl: ModalController,
+    private userData: UserData
+    ) {}
 
   ngOnInit() {
-    this.username = localStorage.getItem('username')
-    this.no_telp_user = localStorage.getItem('no_telp_user')
     this.getData();
+    this.getItemPromo();
+    setInterval(() => {
+      this.getUsername();
+      this.getNoTelpUser();
+    }, 1000)
   }
 
   slideOptsOne = {
@@ -28,9 +41,46 @@ export class DashboardPage implements OnInit {
     speed: 300,
   };
 
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      this.getData();
+      event.target.complete();
+    }, 2000);
+  }
+
+  getUsername() {
+    this.userData.getUsername().then((username) => {
+      this.username = username;
+    });
+  }
+
+  getNoTelpUser() {
+    this.userData.getNoTelpUser().then((no_telp_user) => {
+      this.no_telp_user = no_telp_user;
+    });
+  }
+
+  async account() {
+    let modal = await this.modalCtrl.create({
+      component: AccountPage,
+      cssClass: 'account-modal'
+    });
+    modal.present();
+  }
+
   getData() {
     this.jasttipsDataService.getListCategory().subscribe((rest) => {
       this.listCategory = rest.category;
     });
   }
+
+  getItemPromo() {
+    this.jasttipsDataService.getListItem('5').subscribe((items) => {
+      this.itemPromo = items.item;      
+    })
+  }
+
 }

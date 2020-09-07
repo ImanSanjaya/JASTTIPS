ca896a2d7167c4147118c61c6f3b8bef34629472
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2020-09-07 10:38:30 +0700

    Update: delete unused

--- src/app/api/user-data.ts
+++ src/app/api/user-data.ts
@@ -6,9 +6,8 @@ import { Storage } from '@ionic/storage';
   providedIn: 'root'
 })
 export class UserData {
-  favorites: string[] = [];
+  
   HAS_LOGGED_IN = 'hasLoggedIn';
-  HAS_SEEN_TUTORIAL = 'hasSeenTutorial';
 
   constructor(
     public storage: Storage
